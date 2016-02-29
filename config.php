2829d66ccd59e946002c0670d6941ace71446e6b
noticeZhou <notice_zhou@163.com>
2016-02-29 21:49:14 +0800

    update first

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+	require 'php-sdk/autoload.php';
+
+	class Config
+    {
+        const DB_NAME = 'qspace',
+              DB_USER = 'root',
+              DB_PWD = '****',
+              DB_HOST = '<your_db_host>',
+              ACCESS_KEY = 'g0VBbGZPrMsL9644dxxO0Vv1LxE6U6A8RcEWhC3d',
+              SECRET_KEY = 'tEEAJvotO-0HzM9JTuMtHiL51e1N2zinDu_Wb4ON',
+              BUCKET_NAME = 'photo-view';
+    }
+?>
