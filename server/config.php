c78a88599eb0316569d93b4dec061207ebb39829
noticeZhou <notice_zhou@163.com>
2016-03-24 13:15:40 +0800

    make demo

--- server/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	class Config
-    {
-        const ACCESS_KEY = 'g0VBbGZPrMsL9644dxxO0Vv1LxE6U6A8RcEWhC3d',
-              SECRET_KEY = 'tEEAJvotO-0HzM9JTuMtHiL51e1N2zinDu_Wb4ON',
-              BUCKET_NAME = 'photo-view';
-    }
